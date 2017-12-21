5a6cb621cdc7a9f63656b766faf4794835e5d77f
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-21 04:08:42 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -6,3 +6,12 @@ $('p').on('click', function(){
   alert( "Hey" )}
 );
 }
+
+
+function frameIt(){
+  $(document).ready(function(frameIt){
+  });
+  $('img').on('load', function(){
+  $('img').addClass('tasty');
+});
+}
